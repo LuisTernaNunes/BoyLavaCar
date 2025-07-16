d72a97c00422e7bb9b3b1e6d63c4bc3cb8ff9542
Luis Nunes <luisterna@gmail.com>
2025-07-16 11:43:53 -0300

    first commit

---
+++ src/test/java/br/com/boylavacar/BoyLavaCar/BoyLavaCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boylavacar.BoyLavaCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoyLavaCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
